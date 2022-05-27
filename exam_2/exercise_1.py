768ad578f10937edd08f8203c30102902062f678
Santi <santiagohenn@gmail.com>
2022-05-27 15:55:48 -0300

    Added instructions to exercises

--- exam_2/exercise_1.py
+++ exam_2/exercise_1.py
@@ -1,3 +1,29 @@
+"""
+Diseñaremos un algoritmo de “viralidad” para una conocida red social. 
+
+Dadas dos listas paralelas, una con cantidad de visitas (visitas) y otra con cantidad de “me gusta” (me_gusta) que reciben una serie de
+publicaciones en un día: 
+
+lista_visitas = [1000, 2500, 5000, 150000, 200000]
+lista_me_gusta = [25, 2000, 1000, 50000, 100000]
+
+a) Declarar las listas lista_visitas y lista_me_gusta en nuestro programa principal, debajo de una linea comentario con el texto “Programa principal”.
+b) Nuestra red social determina a cuantos usuarios mostrará la publicacion al dia siguiente con esta formula:
+
+cantidad = me_gusta * ( 1 + me_gusta / visitas ) 
+
+Programar una función llamada obtener_cantidad que tome dos parametros de entrada: visitas y cantidad de me gusta, y retorne un entero (usar int() )
+para la cantidad según la formula anterior. 
+
+d) Programar una funcion llamada computar, que tome dos listas como parametros de entrada: lista_visitas y lista_me_gusta, y devuelva (retorne)
+otra lista, con las cantidades a mostrar para cada par de elementos paralelos. Sugerencia: llamar a la funcion del inciso anterior dentro de esta función.
+
+Imprimir los datos obtenidos. Por cada publicación tendremos un mensaje como el siguiente: “XXXX visitas y XXXX me gusta, mostrar a XXXX usuarios”.
+Puede programar esto en la rutina principal o en una función, como Ud. desee.
+"""
+
+## SOlUCION:
+
 def obtener_cantidad(visitas, me_gusta):
     cantidad = me_gusta * (1 + me_gusta / visitas)
     return int(cantidad)
--- exam_2/exercise_2.py
+++ exam_2/exercise_2.py
@@ -1,3 +1,22 @@
+"""
+Diseñaremos un programa de apuestas. Nuestro programa deberá:
+a) Solicitar al usuario que apueste el valor que tendrá de un dado, es decir, un entero del 1 a 6.
+b) Generar un resultado aleatorio para un dado y determinar si el usuario ganó o no la apuesta.
+Impimir “Gana” o “Pierde” según el resultado del dado y la apuesta.
+c) Ciclar 5 veces, es decir, pedir 5 apuestas. Para cada apuesta, almacenar el resultado en un diccionario,
+donde la clave es el numero de jugada y el dato/valor es una lista o tupla que contenga la jugada del usuario,
+el resultado del dado y un string: “gana” si el usuario gana la apuesta, o “pierde” si el usuario pierde la apuesta.
+c) Programar una función llamada imprimir_resultados que tome de parámetro el diccionario e imprima los resultados de
+la siguiente forma: “El usuario apuesta X, el dado sale Y. El usuario gana/pierde.”. Llamar a esta función
+al final del programa.
+
+Bonus: Si el usuario recibe 5 alfajores cuando gana la apuesta, y pierde 1 alfajor cuando pierde la apuesta, definir
+una función llamada calcular_balance que tome el diccionario de resultados como parametro e imprima el balance de
+alfajores para la ronda de apuestas. Llamar a esta función.
+"""
+
+## SOLUCION
+
 import random as ran
 
 def imprimir_resultados(resultados):
@@ -24,7 +43,7 @@ for i in range(5):
     apuesta = int(input("Apostar al dado: "))
     dado = ran.randint(1,6)
     resultado = "pierde"
-    
+
     if apuesta == dado:
         resultado = "gana"
 
